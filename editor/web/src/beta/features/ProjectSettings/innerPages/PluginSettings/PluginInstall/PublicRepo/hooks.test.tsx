01324f1d574faad0a09a8e934c913ec19233a74b
soneda-yuya <soneda.yuya@gmail.com>
2025-05-14 15:28:21 +0900

    fix: review from piyush

--- editor/web/src/beta/features/ProjectSettings/innerPages/PluginSettings/PluginInstall/PublicRepo/hooks.test.tsx
+++ editor/web/src/beta/features/ProjectSettings/innerPages/PluginSettings/PluginInstall/PublicRepo/hooks.test.tsx
@@ -6,11 +6,9 @@ test("should validate url", () => {
   const wrongUrl1 = "https://google.com";
   const wrongUrl2 = "";
   const wrongUrl3 = "https://github.com";
-  const correctUrl1 = "https://github.com/reearth/reearth-visualizer";
-  const correctUrl2 = "https://github.com/reearth/reearth-visualizer-web";
+  const correctUrl1 = "https://github.com/eukarya-inc/PLATEAU-VIEW";
   expect(validateUrl(wrongUrl1).result).toBe(false);
   expect(validateUrl(wrongUrl2).result).toBe(false);
   expect(validateUrl(wrongUrl3).result).toBe(false);
   expect(validateUrl(correctUrl1).result).toBe(true);
-  expect(validateUrl(correctUrl2).result).toBe(true);
 });
