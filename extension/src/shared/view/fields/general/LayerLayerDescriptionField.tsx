b4ea34a11a73ebb790d70513c285c202cf63ece0
lby <icesunex@hotmail.com>
2025-05-27 11:19:52 +0800

    refactor(extension): improve markdown rendering (#462)

--- extension/src/shared/view/fields/general/LayerLayerDescriptionField.tsx
+++ extension/src/shared/view/fields/general/LayerLayerDescriptionField.tsx
@@ -1,9 +1,9 @@
 import { useAtom } from "jotai";
 import { FC } from "react";
-import Markdown from "react-markdown";
 
 import { ParameterList } from "../../../../prototypes/ui-components";
 import { LayerDescriptionField } from "../../../types/fieldComponents/general";
+import { ViewMarkdownViewer } from "../../../ui-components/common";
 import { CommonContentWrapper } from "../../../ui-components/CommonContentWrapper";
 import { WritableAtomForComponent } from "../../../view-layers/component";
 
@@ -16,18 +16,8 @@ export const LayerLayerDescriptionField: FC<LayerLayerDescriptionFieldProps> = (
   return component.preset?.description ? (
     <ParameterList>
       <CommonContentWrapper>
-        <Markdown skipHtml components={{ a: LinkRenderer }}>
-          {component.preset?.description}
-        </Markdown>
+        <ViewMarkdownViewer content={component.preset?.description} />
       </CommonContentWrapper>
     </ParameterList>
   ) : null;
 };
-
-function LinkRenderer(props: any) {
-  return (
-    <a href={props.href} target="_blank" rel="noreferrer">
-      {props.children}
-    </a>
-  );
-}
